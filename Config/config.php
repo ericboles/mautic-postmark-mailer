e22aa8ddd4ca2e12e7ba253a365df740ec886697
Mario te Best <mario.tebest@waretec.at>
2025-03-07 14:42:07 +0100

    bump

--- Config/config.php
+++ Config/config.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 return [
     'name'        => 'Postmark Plugin',
     'description' => 'Postmark Mailer Plugin for Mautic',
-    'version'     => '1.0.5',
+    'version'     => '1.0.6',
     'author'      => 'Mario te Best',
 ];
